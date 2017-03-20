2d07d807c1591ee7c66844f153b197b6e6b2bd8c
robin <robin_mijeane@hotmail.fr>
2017-03-20 14:37:21 +0100

    id via @mail

--- phpwebsocket/chat.php
+++ phpwebsocket/chat.php
@@ -4,14 +4,15 @@
 require "websocket.class.php";
 require "utilisateur.php";
 
-$listeUtilisateurs=array();
-
-
-
 // Extended basic WebSocket as ChatBot
 class ChatBot extends WebSocket{
   function process($user,$msg){
  
+ 	$listeUtilisateurs=array();
+	$listeUtilisateurs['robin.degironde@gmail.com']= new Utilisateur('robin.degironde@gmail.com', 'DEGIRONDE', 'ROBIN', '1.00000001', '1.000000001', 'mdp', 'X');
+	$listeUtilisateurs['charles.banquet@live.com']= new Utilisateur('charles.banquet@live.com', 'BANQUET', 'CHARLES', '1.00000001', '1.00000001', 'mdp', 'X');
+	$listeUtilisateurs['remi.barbaste@gmail.com']= new Utilisateur('remi.barbaste@gmail.com', 'BARBASTE', 'REMI', '1.00000001', '1.00000001', 'mdp', 'X');
+
     $this->say("< ".$user->socket." :".$msg);
  
  	foreach ( $this->users as $utilisateur ){
@@ -27,8 +28,11 @@ class ChatBot extends WebSocket{
 
 	//decode la data qui est un json de personne
 	$personne= json_decode($res['data'], true);
-	$util= new Utilisateur($user->id, $personne['nom'], $personne['prenom'], $personne['longitude'], $personne['latitude']);
-	echo $util->toString();
+	echo $listeUtilisateurs['robin.degironde@gmail.com']->toString();
+	$listeUtilisateurs['robin.degironde@gmail.com']->setIdSocket($user->id);
+	echo $listeUtilisateurs['robin.degironde@gmail.com']->toString();
+
+
   }
 }
 $master = new ChatBot("localhost",1337);
--- phpwebsocket/utilisateur.php
+++ phpwebsocket/utilisateur.php
@@ -1,18 +1,23 @@
 <?php
 class Utilisateur{
+	//ID=MAIL
 	var $id;
 	var $nom;
 	var $prenom;
 	var $longitude;
 	var $latitude;
+	var $mdp;
+	var $idSocket;
 
-	public function __construct ($pId, $pNom, $pPrenom, $pLongitude, $pLatitude)
+	public function __construct ($pId, $pNom, $pPrenom, $pLongitude, $pLatitude, $pMdp, $pIdSocket)
 	{
 		$this->id=$pId;
 		$this->nom=$pNom;
 		$this->prenom=$pPrenom;
 		$this->longitude=$pLongitude;
 		$this->latitude=$pLatitude;
+		$this->idSocket=$pIdSocket;
+		$this->mdp=$pMdp;
 	}
 
 	public function getJson()
@@ -20,9 +25,14 @@ class Utilisateur{
 		return json_encode(get_object_vars($this));
 	}
 
+	public function setIdSocket($pIdSocket)
+	{
+		$this->idSocket=$pIdSocket;
+	}
+
 	public function toString()
 	{
-		return $this->id.' '.$this->nom.' '.$this->prenom.' '.$this->longitude.' '.$this->latitude."\n";
+		return $this->id.' '.$this->nom.' '.$this->prenom.' '.$this->longitude.' '.$this->latitude.' '.$this->mdp.' '.$this->idSocket."\n";
 	}
 }
 ?>
