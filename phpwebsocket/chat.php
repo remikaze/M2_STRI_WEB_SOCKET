c5338148a098a0c7baa64e97dbe20987747b708d
remi barbaste <remi.b7@hotmail.fr>
2017-03-21 18:10:47 +0100

    Ajout serveur  + maj du client

---
+++ phpwebsocket/chat.php
@@ -0,0 +1,119 @@
+#!/usr/bin/php -q
+<?php
+// Run from command prompt > php -q chatbot.demo.php
+require "websocket.class.php";
+
+
+// Extended basic WebSocket as ChatBot
+class ChatBot extends WebSocket{
+
+	function getUtilisateurByUserId($pUserId)
+	{
+		foreach ( $this->listeUtilisateurs as $utilisateur ){
+			if($utilisateur->idSocket == $pUserId)
+			{
+				return $utilisateur;
+			}
+		}
+	}
+
+  function process($user,$msg){
+ 
+ 	//ECHO
+    $this->say("< ".$user->socket." :".$msg);
+    $this->send($user->socket,$msg);
+
+	// //Envoyer a tous les utilisateurs 	
+ // 	foreach ( $this->users as $utilisateur ){
+	// 	$this->send($utilisateur->socket,$msg);
+	// }
+
+	
+	echo "JSON TO ARRAY\n";
+	//Decode le message entier et recupere la commande
+	$res = json_decode($msg, true);
+	echo $res['commande']."\n";
+	$commande=$res['commande'];
+
+	//DECODE la data qui est un json de personne
+	$personne = json_decode($res['data'], true);
+
+	switch ($commande) {
+		case "CONNECT":
+			$this->listeUtilisateurs[$personne['id']]->setIdSocket($user->id);
+			$this->listeUtilisateurs[$personne['id']]->setLongitude($personne['longitude']);
+			$this->listeUtilisateurs[$personne['id']]->setLatitude($personne['latitude']);
+			$this->listeUtilisateurs[$personne['id']]->setSocket($user->socket);
+
+			break;
+		
+		default:
+			echo "COMMANDE INCONNUE: $commande\n";
+			break;
+	}
+
+
+	//Retrouver qui a initialise la fonction
+	$utilisateurSocket= $this->getUtilisateurByUserId($user->id);
+
+	//VOIR SI LUTILISATEUR EST PRES
+	foreach ( $this->listeUtilisateurs as $utilisateur){
+		//PREVENIR L'UTILISATEUR
+		if($utilisateur->getId() != $utilisateurSocket->id)
+		{
+			if($this->listeUtilisateurs[$utilisateurSocket->id]->estPres($utilisateur))
+			{
+				//MESSAGE A LUTILISATEUR QUI VIENT DE SE CONNECTER
+				$retour= $utilisateur->prenom." est pres de vous: ".$this->listeUtilisateurs[$utilisateurSocket->id]->distance($utilisateur)."km\n";
+				echo $retour;
+
+				$this->say("< ".$user->socket." :".$msg);
+	 			$this->send($user->socket,$retour);
+
+	 			//MESSAGE A L AUTRE UTILISATEUR
+				$retour= $this->listeUtilisateurs[$utilisateurSocket->id]->prenom." est pres de vous: ".$this->listeUtilisateurs[$utilisateurSocket->id]->distance($utilisateur)."km\n";
+				echo $retour;
+
+				$this->say("< ".$utilisateur->socket." :".$msg);
+	 			$this->send($utilisateur->socket,$retour);
+			}
+		}
+	}
+
+	//VOIR SI utilSocket pratique les même sports que quelqu'un d'autre
+	foreach ( $this->listeUtilisateurs as $vUtilisateur){
+		//VERIF QUE CE NEST PAS LE MEME USER
+		if($vUtilisateur->getId() != $utilisateurSocket->id)
+		{
+			//PARCOURIR TOUS LES SPORTS DE utilisateurSocket
+			foreach (array_keys($utilisateurSocket->sports) as $vSport) {
+				//REGARDER SI, AU MOINS vUtilisateur pratique le même sport que utilisateurSocket
+				if(array_key_exists($vSport, $vUtilisateur->sports)){
+					$intersect=array_intersect($utilisateurSocket->sports[$vSport], $vUtilisateur->sports[$vSport]);
+					foreach ($intersect as $vDate) {
+						$retour= $vUtilisateur->prenom." est disponible pour faire: ".$vSport." le ".$vDate." sa distance: ".$this->listeUtilisateurs[$utilisateurSocket->id]->distance($vUtilisateur)."km\n";
+						$this->say("< ".$user->socket." :".$retour);
+	 					$this->send($user->socket,$retour);
+					}
+				}
+			}
+
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+	foreach ( $this->listeUtilisateurs as $utilisateur ){
+		echo $utilisateur->toString();
+		echo $utilisateur->sportsToString();
+	}
+
+	}
+}
---
+++ phpwebsocket/utilisateur.php
@@ -0,0 +1,146 @@
+<?php
+class Utilisateur{
+	//ID=MAIL
+	var $id;
+	var $nom;
+	var $prenom;
+	var $longitude;
+	var $latitude;
+	var $mdp;
+	var $idSocket;
+	var $socket;
+	var $sports;
+
+	public function __construct ($pId, $pNom, $pPrenom, $pLongitude, $pLatitude, $pMdp, $pIdSocket)
+	{
+		$this->id=$pId;
+		$this->nom=$pNom;
+		$this->prenom=$pPrenom;
+		$this->longitude=$pLongitude;
+		$this->latitude=$pLatitude;
+		$this->idSocket=$pIdSocket;
+		$this->mdp=$pMdp;
+		$this->sports=array();
+	}
+
+	public function getJson()
+	{
+		return json_encode(get_object_vars($this));
+	}
+
+	public function getLongitude()
+	{
+		return $this->longitude;
+	}
+
+	public function getLatitude()
+	{
+		return $this->latitude;
+	}
+
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	public function getNom()
+	{
+		return $this->nom;
+	}
+
+	public function getPrenom()
+	{
+		return $this->prenom;	
+	}
+
+	public function setIdSocket($pIdSocket)
+	{
+		$this->idSocket=$pIdSocket;
+	}
+
+	public function setSocket($pSocket)
+	{
+		$this->socket=$pSocket;
+	}
+
+	public function setLongitude($pLongitude)
+	{
+		$this->longitude=$pLongitude;
+	}
+
+	public function setLatitude ($pLatitude)
+	{
+		$this->latitude=$pLatitude;
+	}
+
+	public function toString()
+	{
+		return $this->id.' '.$this->nom.' '.$this->prenom.' '.$this->longitude.' '.$this->latitude.' '.$this->mdp.' '.$this->idSocket."\n";
+	}
+
+	public function distance($pUtilisateur) 
+	{
+		$alt1=0;
+		$alt2=0;
+
+		$lat1=$this->latitude;
+		$lon1=$this->longitude;
+
+		$lat2= $pUtilisateur->getLatitude();
+		$lon2= $pUtilisateur->getLongitude();
+
+		//rayon de la terre
+		$r = 6366;
+		$lat1 = deg2rad($lat1);
+		$lat2 = deg2rad($lat2);
+		$lon1 = deg2rad($lon1);
+		$lon2 = deg2rad($lon2);
+ 
+		//recuperation altitude en km
+		$alt1 = $alt1/1000;
+		$alt2 = $alt2/1000;
+ 
+		//calcul précis
+		$dp= 2 * asin(sqrt(pow (sin(($lat1-$lat2)/2) , 2) + cos($lat1)*cos($lat2)* pow( sin(($lon1-$lon2)/2) , 2)));
+ 
+		//sortie en km
+		$d = $dp * $r;
+ 
+		//Pythagore a dit que :
+		 $h = sqrt(pow($d,2)+pow($alt2-$alt1,2));
+ 
+		return $h;
+	}
+
+	public function estPres($pUtilisateur)
+	{
+		$critere= 80;
+		return($this->distance($pUtilisateur) <= $critere);
+	}
+
+	public function addSport($pSport, $pDate)
+	{
+		if(!array_key_exists($pSport, $this->sports))
+		{
+			$this->sports[$pSport]= array();
+		}
+
+		array_push($this->sports[$pSport], $pDate);
+	}
+
+	public function sportsToString()
+	{
+		$retour="";
+		foreach ( array_keys($this->sports) as $nomSport ){
+			$retour=$retour.$nomSport."\n";
+			
+			foreach ( $this->sports[$nomSport] as $date ){
+				$retour=$retour.' '.$date;
+			}
+			$retour=$retour."\n";
+		}
+		return $retour;
+	}
+
+}
+?>
